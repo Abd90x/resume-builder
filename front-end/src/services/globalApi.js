3fc33e9f9b3dc76fbe24f2305dc47f1baad0a104
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2024-11-25 00:01:44 +0300

    Global API config

--- front-end/src/services/globalApi.js
+++ front-end/src/services/globalApi.js
@@ -1,33 +1,11 @@
-import { data } from "autoprefixer";
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const API_BASE_URL = import.meta.env.VITE_STRAPI_BASE_URL;
 
-const axiosClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${API_KEY}`,
-  },
-});
-
-const CreateNewResume = (data) => axiosClient.post("/user-resumes", data);
-
-const GetUserResumes = (userEmail) =>
-  axiosClient.get(`/user-resumes?filters[userEmail][$eq]=${userEmail}`);
-
-const GetResumeById = (id) => axiosClient.get(`/user-resumes/${id}?populate=*`);
-
-const UpdateResumeDetail = (data, id) =>
-  axiosClient.put(`/user-resumes/${id}`, data);
-
-const DeleteResume = (id) => axiosClient.delete(`/user-resumes/${id}`);
-
-export default {
-  CreateNewResume,
-  GetUserResumes,
-  UpdateResumeDetail,
-  GetResumeById,
-  DeleteResume,
+axios.defaults.headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${API_KEY}`,
 };
+
+axios.defaults.baseURL = API_BASE_URL;
