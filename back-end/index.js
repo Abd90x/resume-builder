d911e6f25c3f1c59b4ab4e5840b27daf7f341c79
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-01-05 21:14:00 +0300

    Revert "add vercel"
    This reverts commit d7da4f7b351be9ccc9d4903a3146030854d3b0ab.

--- back-end/index.js
+++
@@ -1,5 +0,0 @@
-const { createServer } = require("http");
-const strapi = require("@strapi/strapi");
-
-const server = strapi().start();
-createServer(server.callback()).listen(process.env.PORT || 3000);
